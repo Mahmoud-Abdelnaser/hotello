ec0917886feff4e4275706586ec2c989ff3af241
Mahmoud-Abdelnaser <95445076+Mahmoud-Abdelnaser@users.noreply.github.com>
2023-04-01 17:54:27 +0200

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const mysql=require('mysql')
 const db=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"1114524",
-    database:"hotello"
+     host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.MYSQL_DB
 });
 db.connect((err)=>{
     if(err)
